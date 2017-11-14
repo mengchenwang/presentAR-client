419bf9bfe3677d7dfaca8c030d6a99e4277b8939
Luan van Pletsen <luanvanpletsen@msn.com>
2017-11-14 16:36:59 +0000

    Refactored AssetHandler.cs to TransformAsset function

--- Assets/Scripts/AssetHandler.cs
+++ Assets/Scripts/AssetHandler.cs
@@ -14,10 +14,17 @@ public class AssetHandler : MonoBehaviour {
 		
 	}
 
-	public void LoadAsset (AssetBundle bundle, string modelName) {
+	public void LoadAsset (AssetBundle bundle, string modelName) 
+	{
 		GameObject model = (GameObject)bundle.LoadAsset (modelName);
 		GameObject modelInstance = Instantiate (model);
+		TransformAsset (modelInstance);
+	}
+
+	public void TransformAsset (GameObject modelInstance)
+	{
 		modelInstance.transform.parent = GameObject.Find ("ImageTarget").transform;
 		modelInstance.transform.localScale = new Vector3 (0.002F, 0.002F, 0.002F);
 	}
+		
 }
